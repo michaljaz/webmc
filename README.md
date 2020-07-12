1748314f7ea0aa878fb8c398dd7f2e2c74f96428
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2020-07-12 21:19:13 +0200

    totalLocal

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # web-minecraft
 Render minecrafta w przeglądarce
-<br><br>
-<img src="screenshotx.png"
-     alt="Screenshot"
-     style="float: left; margin-right: 10px;" />
-     <br><br>
-<img src="screenshotxx.png"
-     alt="Screenshot"
-     style="float: left; margin-right: 10px;" />
-     <br><br>
-<h1>DEMO</h1>
-Zobacz: <a href="https://michaljaz.github.io/web-minecraft/">DEMO</a>
+<h1>Instalacja</h1>
+
+```bash
+#Instalacja repozytorium
+git clone https://github.com/michaljaz/web-minecraft
+
+#Instalacja i uruchomienie serwera node.js 
+#(Proces 1)
+npm install -g http-server
+http-server web-minecraft/
+
+#Uruchomienie serwera gry socket.io 
+#(Proces 2, ten proces musisz wywołać w osobnej zakładce: np. klikając Ctrl+Shift+T)
+node web-minecraft/server/server.js
+
+```
+
+<h1>Jak uruchomić grę?</h1>
+Aby włączyć grę wystarczy, że wejdziesz w link: <a href="http://localhost:8080/">http://localhost:8080/</a>.<br>
+<i>(Najpierw oczywiście musisz wykonać kroki podane powyżej)</i>
---
+++ server/server.js
@@ -0,0 +1,48 @@
+var server = require("http").createServer();
+var io = require("socket.io")(server);
+
+var world={};
+startUp();
+function startUp(){
+  var vari=50
+  for (let y = 0; y < vari; ++y) {
+    for (let z = 0; z < vari; ++z) {
+      for (let x = 0; x < vari; ++x) {
+        const height = (Math.sin(x / vari * Math.PI * 2) + Math.sin(z / vari * Math.PI * 3)) * (vari / 6) + (vari / 2);
+        if (y < height) {
+          world[`${x}:${y}:${z}`]=2;
+        }
+      }
+    }
+  }
+}
+
+var players={};
+
+io.sockets.on("connection", function(socket) {
+  console.log("User connected: "+socket.id)
+  socket.on("playerUpdate",function (data){
+    players[socket.id]=data
+    io.sockets.emit("playerUpdate",players)
+  })
+  socket.emit("firstLoad",world)
+  socket.on("blockUpdate",function (block){
+    block[0]=Math.floor(block[0])
+    block[1]=Math.floor(block[1])
+    block[2]=Math.floor(block[2])
+    world[`${block[0]}:${block[1]}:${block[2]}`]=block[3];
+    if(block[3]==0){
+      delete world[`${block[0]}:${block[1]}:${block[2]}`]
+      console.log("Block removed",`${block[0]}:${block[1]}:${block[2]}`)
+    }else{
+      console.log("Block placed",`${block[0]}:${block[1]}:${block[2]}`)
+    }
+    io.sockets.emit("blockUpdate",block)
+  })
+  socket.on("disconnect", function() {
+    console.log("User disconnected: " + socket.id);
+    delete players[socket.id]
+  });
+});//
+
+server.listen(35565);
