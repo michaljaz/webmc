2e02eed02419ea8189f93ce35e6fcc3413d1c357
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2020-07-13 21:49:58 +0200

    nodeFix

--- nodejs/run.js
+++ nodejs/run.js
@@ -56,7 +56,7 @@ io.sockets.on("connection", function(socket) {
 server1.listen(socketPort);
 
 //Webserver
-var serve = serveStatic("../");
+var serve = serveStatic("./../");
 
 var server2 = http.createServer(function(req, res) {
   var done = finalhandler(req, res);
