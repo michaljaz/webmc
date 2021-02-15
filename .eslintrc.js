872cfb84006ea8c6fc12e4bdd9631cecdc1087af
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-02-15 14:50:51 +0100

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": [
+      "eslint:recommended",
+      "plugin:prettier/recommended"
+    ],
+    "env": {
+      "es6": true,
+      "node": true
+    },
+    "parserOptions": {
+      "ecmaVersion": 8
+    }
+  };
