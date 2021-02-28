3109273293459445f7e00fcae40c3f45ac96428f
NexusNull <p.wellershaus@googlemail.com>
2021-02-28 15:22:00 +0100

    add the minecraft font and use it as default

--- src/client/webpack.common.js
+++ src/client/webpack.common.js
@@ -72,6 +72,14 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
     ],
   },
   plugins: [
