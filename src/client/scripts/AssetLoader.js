590deaf847a0a0212a8b3c8dc57673d67c30427e
NexusNull <p.wellershaus@googlemail.com>
2021-03-01 17:10:17 +0100

    use Map instead of Object property assignment

--- src/client/scripts/AssetLoader.js
+++ src/client/scripts/AssetLoader.js
@@ -7,75 +7,72 @@ class AssetLoader {
     }
 
     async init() {
-        return new Promise(async (resolve) => {
-            let assets = await $.get("assets/assetLoader.json");
-            await this.load(assets, () => {
-                console.log("AssetLoader: done loading!");
-                resolve()
-            });
-        })
+        let assets = await $.get("assets/assetLoader.json");
+        await this.load(assets);
+        console.log("AssetLoader: done loading!");
     }
 
-    async load(assets, callback) {
+    async load(assets) {
+        return new Promise((resolve) => {
+            var textureLoader = new THREE.TextureLoader();
+            var fbxl = new FBXLoader();
+            var assetsNumber = Object.keys(assets).length;
+            var assetsLoaded = 0;
 
-        var textureLoader = new THREE.TextureLoader();
-        var fbxl = new FBXLoader();
-        var assetsNumber = Object.keys(assets).length;
-        var assetsLoaded = 0;
+            for (const assetName in assets) {
+                if (!assets.hasOwnProperty(assetName))
+                    continue;
 
-        for (const assetName in assets) {
-            if(!assets.hasOwnProperty(assetName))
-                continue;
+                let asset = assets[assetName];
 
-            let asset = assets[assetName];
-            var img;
 
-            switch (asset.type) {
-                case "texture":
-                    textureLoader.load(asset.path, (texture) => {
-                        this.assets[assetName] = texture;
-                        assetsLoaded++;
-                        if (assetsLoaded === assetsNumber) {
-                            return callback();
-                        }
-                    });
-                    break;
-                case "text":
-                    $.get(asset.path, (data) => {
-                        this.assets[assetName] = data;
-                        assetsLoaded++;
-                        if (assetsLoaded === assetsNumber) {
-                            return callback();
-                        }
-                    });
-                    break;
-                case "image":
-                    img = new Image();
-                    img.onload = () => {
-                        this.assets[assetName] = img;
-                        assetsLoaded++;
-                        if (assetsLoaded === assetsNumber) {
-                            return callback();
-                        }
-                    };
-                    img.src = asset.path;
-                    break;
-                case "fbx":
-                    fbxl.load(asset.path, (fbx) => {
-                        this.assets[assetName] = fbx;
-                        assetsLoaded++;
-                        if (assetsLoaded === assetsNumber) {
-                            return callback();
-                        }
-                    });
-                    break;
+                switch (asset.type) {
+                    case "texture":
+                        textureLoader.load(asset.path, (texture) => {
+                            this.assets.set(assetName, texture);
+                            assetsLoaded++;
+                            if (assetsLoaded === assetsNumber) {
+                                return resolve();
+                            }
+                        });
+                        break;
+                    case "text":
+                        $.get(asset.path, (data) => {
+                            this.assets.set(assetName, data);
+                            assetsLoaded++;
+                            if (assetsLoaded === assetsNumber) {
+                                return resolve();
+                            }
+                        });
+                        break;
+                    case "image":
+                        let img = new Image();
+                        img.onload = () => {
+                            this.assets.set(assetName, img);
+                            assetsLoaded++;
+                            if (assetsLoaded === assetsNumber) {
+                                return resolve();
+                            }
+                        };
+                        img.src = asset.path;
+                        break;
+                    case "fbx":
+                        fbxl.load(asset.path, (fbx) => {
+                            this.assets.set(assetName, fbx);
+                            assetsLoaded++;
+                            if (assetsLoaded === assetsNumber) {
+                                return resolve();
+                            }
+                        });
+                        break;
+                }
             }
-        }
-        return this;
+        })
+
     }
 
     get(assetName) {
-        return this.assets[assetName];
+        return this.assets.get(assetName);
     }
 }
 
